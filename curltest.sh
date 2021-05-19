6d82f4bbc2bc8b824ccf98a6fe31471ff2b30e76
j t f <rrmutt@gmail.com>
2021-05-19 10:57:08 -0700

    checkpoint

--- curltest.sh
+++ curltest.sh
@@ -2,9 +2,8 @@
 
 while [ 1 == 1 ]
 do
-    curl -s http://192.168.1.145:8000/?aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555 > /dev/null
+    curl -s http://192.168.1.149:8000/?aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555
     sleep 0.2
-    curl -s http://192.168.1.145:8000/?5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa5555aaaa > /dev/null
-    sleep 0.2
+
 
 done
